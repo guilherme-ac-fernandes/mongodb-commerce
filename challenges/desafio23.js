1c564f2d29c0ae99a1a04e8875af32678b85863b
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-17 16:33:32 -0300

    feat: resolução do desafio 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {},
+  { $push: {
+    tags: {
+      $each: ["combo", "tasty"],
+      $sort: 1,
+    },
+  },
+  },
+);
+
+db.produtos.find({}, {
+  nome: true,
+  _id: false,
+  tags: true,
+});
