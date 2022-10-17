04f46753610c319d976435cfcea77152850ff119
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-17 17:17:02 -0300

    feat: resolução do desafio 29

---
+++ challenges/desafio29.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany(
+  {},
+  {
+    $rename: { descricao: "descricaoSite" },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: true,
+    _id: false,
+    descricaoSite: true,
+  },
+);
