f215d9c5feaa830215ba0c22edbd1b3d3408d93f
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-17 16:41:47 -0300

    feat: resolução do desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      valoresNutricionais: {
+        $each: [],
+        $sort: { percentual: -1 },
+      },
+    },
+  },
+);
+
+db.produtos.find({}, {
+  nome: true,
+  _id: false,
+  valoresNutricionais: true,
+});
